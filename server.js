2d8efb7c202ed6686b0810870ec42873e4492a19
maiconmoretto <maiconmorettos@gmail.com>
2020-01-20 18:00:04 -0200

    LEasson 7. Registering GraphQL with Express

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+        graphiql: true
+}));
+
 app.listen(4000, () => {
-    console.log('listening ;) ')
+    console.log('listening  ');
 });
